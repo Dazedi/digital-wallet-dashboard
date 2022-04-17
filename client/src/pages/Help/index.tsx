bb18ab71952190163b750c8c01de31b3d7d1e470
Tommi Vepsae <vepsa.tommi@gmail.com>
2022-04-18 01:44:58 +0900

    memo

--- client/src/pages/Help/index.tsx
+++ client/src/pages/Help/index.tsx
@@ -5,11 +5,6 @@ import {
   List,
   ListItem,
   Divider,
-  Table,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableCell,
 } from '@mui/material';
 
 export const Help = () => {
@@ -29,36 +24,32 @@ export const Help = () => {
       <Box sx={{ border: '1px solid #ccc', my: 2, p: 2 }}>
         <Typography variant="h5">Client</Typography>
         <Typography variant="body1">
-          Running on React 18, using Vite. UI components from Material UI. Makes
-          requests to server with Axios.
+          Vite + React + Material UI + React 18<br/>
+          Makes API requests to server with Axios.<br/>
+          First time testing Vite, seems to be quite nice
         </Typography>
         <Typography variant="h5" sx={{ marginTop: 1 }}>
           Server
         </Typography>
         <Typography variant="body1">
+          NodeJS + NestJS. <br/>
+          Currency exchange data is stored in-memory. <br/>
+          Wallet information is fetched from etherscan.io.<br/>
+          First time using NestJS (seemed interesting and decided to try it for this task)
+        </Typography>
+      </Box>
+      <Box sx={{ border: '1px solid #ccc', my: 2, p: 2 }}>
+        <Typography variant="h5">Memo</Typography>
+        <Typography variant="body1">
+          The original client design seemed to be using icons similar to font awesome icons. <br/>
+          I chose to use similar icons from material icons instead.<br/><br/>
+          Also the select menu for currency seemed to use the arrow display that is common in number inputs (weird in this case). 
+          So I used the default MUI Select display.
+        </Typography>
+        <Typography variant="body1">
           NodeJS + NestJS. Currency exchange data is stored in-memory. Wallet
           information is fetched from etherscan.io.
         </Typography>
-        {/* <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>a</TableCell>
-              <TableCell>b</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell>a</TableCell>
-              <TableCell>b</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>a</TableCell>
-              <TableCell>b</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-        
-        <Typography variant="body1">Infrastructure etc</Typography>    */}
       </Box>
     </Page>
   );
